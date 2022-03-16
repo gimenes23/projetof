bd24514ee04eb89ad820d5a522d8430d63958eb1
Mateus de Barros Gimenes <mateusgimenes23@gmail.com>
2022-03-16 11:22:58 -0300

    commit final

---
+++ carrosel.js
@@ -0,0 +1,25 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    draggable: true,
+    // dots: '.dots',
+    arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+    }
+});
+
+new Glider(document.querySelector('.glider-xbox'), {
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    draggable: true,
+    // dots: '.dots',
+    arrows: {
+        prev: '.glider-prev-xbox',
+        next: '.glider-next-xbox'
+    }
+});
+
+
+
+
